92b3558a7ce23089f640ff2bc195cc5bf13f833f
sam-fsd <kabirusam037@gmail.com>
2024-01-03 10:28:49 +0000

    eslint compliance

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true;
 
   if (trueOrFalse) {
-    task = task;
-    task2 = task2;
+    task = false;
+    task2 = true;
   }
 
   return [task, task2];
