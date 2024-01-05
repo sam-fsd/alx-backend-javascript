ce5834710ffeb727f9e220325a1b807d963f3a4d
sam-fsd <kabirusam037@gmail.com>
2024-01-05 22:37:28 +0000

    refactor

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then((result) => {
-      console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
-    })
-    .catch(() => console.log('Signup system offline'));
+  const x = uploadPhoto();
+  const y = createUser();
+  return Promise.all([x, y]).then(
+    (data) => console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`),
+    () => console.log('Signup system offline'),
+  );
 }
