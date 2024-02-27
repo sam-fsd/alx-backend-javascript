3ee61ac66d8cf597a7b9bd8b6a94f37687601745
sam-fsd <kabirusam037@gmail.com>
2024-02-27 16:53:37 +0300

    refactor for a different approach

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,40 +1,43 @@
+// Read database file synchronously
+// Log number of students in database
+// Log number of students in each field as well as their names
 const fs = require('fs');
 
-const countStudents = (filePath) => {
-  try {
-    const data = fs.readFileSync(filePath, 'utf8');
-    const rows = [];
-    const CSList = [];
-    const SWEList = [];
-
-    data.split('\n').forEach((line, index) => {
-      // Skip the header line if needed
-      if (index === 0) return;
-
-      // Split the line by comma (or any delimiter you're using)
-      const values = line.split(',');
-
-      // Assuming each line has the same number of columns
-      rows.push(values);
-    });
-
-    rows.forEach((student, index) => {
-      if (student[3] === 'CS') {
-        CSList.push(student[0]);
-      } else {
-        SWEList.push(student[0]);
-      }
-    });
-
-    console.log(`Number of studentS: ${rows.length}`);
-    console.log(
-      `Number of students in CS: ${CSList.length} List: ${CSList.join(', ')}`
-    );
-    console.log(
-      `Number of students in CS: ${SWEList.length} List: ${SWEList.join(', ')}`
-    );
-  } catch (err) {
+const countStudents = (path) => {
+  // Make sure can read file
+  if (!fs.existsSync(path)) {
     throw new Error('Cannot load the database');
+  }
+  // Get content of file synchronously
+  const content = fs.readFileSync(path);
+  // Convert to string, split by newline
+  let lines = content.toString().split('\n');
+  // Filter removes any empty lines, slice removes header
+  lines = lines.filter((line) => line !== '').slice(1);
+  console.log(`Number of students: ${lines.length}`);
+
+  // Get list of every "field" field in file
+  const field = lines.map((line) => line.split(',')[3]);
+  // Get unique values of "field" field
+  const eachField = [...new Set(field)];
+
+  for (const fieldName of eachField) {
+    // Return list of students for each field
+    const studentsPerField = lines
+      // This only works because field is last element in line
+      // Filter into data for each "field" field
+      .filter((line) => line.endsWith(fieldName))
+      .map((line) => {
+        // Split each line by comma into array
+        const stdnt = line.split(',');
+        // Return student name (first index)
+        return stdnt[0];
+      });
+    console.log(
+      `Number of students in ${fieldName}: ${
+        studentsPerField.length
+      }. List: ${studentsPerField.join(', ')}`
+    );
   }
 };
 
