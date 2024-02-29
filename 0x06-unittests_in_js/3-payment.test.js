4e4ac860890c03cf8b85daf6cc2706d0b1ddb678
sam-fsd <kabirusam037@gmail.com>
2024-02-29 20:59:37 +0300

    use chai assertion

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -2,15 +2,14 @@ const { it, describe } = require('mocha');
 const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
 const sinon = require('sinon');
-const assert = require('assert');
-
+const expect = require('chai').expect;
 describe('sendPaymentRequestToApi', function () {
   it('check that Utils.calculateNumber was called once', function () {
     const spy = sinon.spy(Utils, 'calculateNumber');
 
     sendPaymentRequestToApi(100, 20);
 
-    assert(spy.calledOnce);
+    expect(spy.calledOnce).to.be.true;
     spy.restore();
   });
 });
