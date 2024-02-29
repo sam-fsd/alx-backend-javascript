bd8f169ff7dc5766c9299a7030c35107cb56419c
sam-fsd <kabirusam037@gmail.com>
2024-02-29 20:56:05 +0300

    refactor

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -8,7 +8,7 @@ describe('sendPaymentRequestToApi', function () {
   it('check that Utils.calculateNumber was called once', function () {
     const spy = sinon.spy(Utils, 'calculateNumber');
 
-    sendPaymentRequestToApi(50, 24.52);
+    sendPaymentRequestToApi(100, 20);
 
     assert(spy.calledOnce);
     spy.restore();
